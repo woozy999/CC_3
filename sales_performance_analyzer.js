f918390ec7c7f898682d525ceb11764ea7761a61
k <kyle.offenbacher@gmail.com>
2024-09-08 16:08:09 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -10,3 +10,21 @@ function calculateAverageSales(salesArray) {
 
     return averageSales;
 }
+
+//part 2
+
+function determinePerformnaceRating(averageSales) {
+
+    if (averageSales > 10000) {
+        return "Excellent";
+    }
+    else if (averageSales >= 7000 && averageSales <= 10000) {
+        return "Good";
+    }
+    else if (averageSales >= 4000 && averageSales <= 7000) {
+        return "Satisfactory";
+    }
+    else {
+        return "Needs Improvement";
+    }
+}
